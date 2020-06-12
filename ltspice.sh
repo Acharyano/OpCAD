5884d7e63b0e16a0d741dce3812442abad98e0a5
Acharyano <66840847+Acharyano@users.noreply.github.com>
2020-06-12 12:33:05 -0400

    Adapted to the new lt spice vs

--- ltspice.sh
+++ ltspice.sh
@@ -4,13 +4,13 @@
 sudo apt-get install wine
 # sudo pacman -S wine
 cd /tmp/
-wget http://ltspice.linear-tech.com/software/LTspiceIV.exe
-wine LTspiceIV.exe
-rm LTspiceIV.exe
+wget http://ltspice.linear-tech.com/software/LTspiceXVII.exe #XVII can be changed to the
+wine LTspiceXVII.exe                                         #the latest version
+rm LTspiceXVII.exe
 
 # start lstpice through wine
-wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceIV/scad3.exe
+wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceXVII/scad3.exe
 
 # start ltspice through wine with /tmp/example.asc
-wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceIV/scad3.exe /tmp/example.asc
+wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceXVII/scad3.exe /tmp/example.asc
 
